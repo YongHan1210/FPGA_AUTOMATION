728520c40c059c614135ddfe0dc29cf480ba47cc
Yong Han <yonghan.lee@starfive.com>
2022-12-01 14:30:29 +0800

    ok

--- nono.py
+++ nono.py
@@ -8,21 +8,21 @@ class yamld:
     def __init__(self,modulename):
         self.modulename=modulename
         
-        self.hostname=self.getdata()[self.modulename]["hostname"]
+        self.hostname=self.getdata()[self.modulename]["fpga"]["hostname"]
         self.powermodule_ip=self.def_powermodule_ip()
-        self.power_1_8V=self.getdata()[self.modulename]["power_1_8V"]
-        self.power_3_3V=self.getdata()[self.modulename]["power_3_3V"]
-        self.power_5_0V=self.getdata()[self.modulename]["power_5_0V"]
-        self.S2CCLK_1=self.getdata()[self.modulename]["S2CCLK_1"]
-        self.S2CCLK_2=self.getdata()[self.modulename]["S2CCLK_2"]
-        self.S2CCLK_3=self.getdata()[self.modulename]["S2CCLK_3"]
-        self.S2CCLK_4=self.getdata()[self.modulename]["S2CCLK_4"]
-        self.S2CCLK_5=self.getdata()[self.modulename]["S2CCLK_5"]
-        self.S2CCLK_6=self.getdata()[self.modulename]["S2CCLK_6"]
-        self.S2CCLK_7=self.getdata()[self.modulename]["S2CCLK_7"]
-        self.S2CCLK_8=self.getdata()[self.modulename]["S2CCLK_8"]
-        self.fpga1_bitfile=self.getdata()[self.modulename]["fpga1_bitfile"]
-        self.fpga2_bitfile=self.getdata()[self.modulename]["fpga2_bitfile"]
+        self.power_1_8V=self.getdata()[self.modulename]["fpga"]["power_1_8V"]
+        self.power_3_3V=self.getdata()[self.modulename]["fpga"]["power_3_3V"]
+        self.power_5_0V=self.getdata()[self.modulename]["fpga"]["power_5_0V"]
+        self.S2CCLK_1=self.getdata()[self.modulename]["fpga"]["S2CCLK_1"]
+        self.S2CCLK_2=self.getdata()[self.modulename]["fpga"]["S2CCLK_2"]
+        self.S2CCLK_3=self.getdata()[self.modulename]["fpga"]["S2CCLK_3"]
+        self.S2CCLK_4=self.getdata()[self.modulename]["fpga"]["S2CCLK_4"]
+        self.S2CCLK_5=self.getdata()[self.modulename]["fpga"]["S2CCLK_5"]
+        self.S2CCLK_6=self.getdata()[self.modulename]["fpga"]["S2CCLK_6"]
+        self.S2CCLK_7=self.getdata()[self.modulename]["fpga"]["S2CCLK_7"]
+        self.S2CCLK_8=self.getdata()[self.modulename]["fpga"]["S2CCLK_8"]
+        self.fpga1_bitfile=self.getdata()[self.modulename]["fpga"]["fpga1_bitfile"]
+        self.fpga2_bitfile=self.getdata()[self.modulename]["fpga"]["fpga2_bitfile"]
         
     def getdata(self):
         with open("hello.yaml","r") as f:
@@ -593,17 +593,16 @@ class runcall:
 #     powermoduleip=yamld.getpowermodule_ip()
 #     print(powermoduleip)
 #     listvar=[yamld.getpower_1_8V(),yamld.getpower_3_3V(),yamld.getpower_5_0V(),yamld.getS2CCLK_1(),yamld.getS2CCLK_2(),yamld.getS2CCLK_3(),yamld.getS2CCLK_4(),yamld.getS2CCLK_5(),yamld.getS2CCLK_6(),yamld.getS2CCLK_7(),yamld.getS2CCLK_8(),yamld.getfpga1_bitfile(),yamld.getfpga2_bitfile()]
+# #     # mk=mkfolfildir(yamld.getfpga1_bitfile(),yamld.getfpga2_bitfile())
+# #     # fpga1outpath,fpga2outpath=mk.mainff()
     
-#     # mk=mkfolfildir(yamld.getfpga1_bitfile(),yamld.getfpga2_bitfile())
-#     # fpga1outpath,fpga2outpath=mk.mainff()
-    
-#     # program_retry=1
-#     # auto=automationmain(modulename,hostname,powermoduleip,listvar,fpga1outpath,fpga2outpath)
-#     # #auto=automationmain(powermoduleip,hostname,yamld.getpower_1_8V(),yamld.getpower_3_3V(),yamld.getpower_5_0V(),yamld.getS2CCLK_1(),yamld.getS2CCLK_2(),yamld.getS2CCLK_3(),yamld.getS2CCLK_4(),yamld.getS2CCLK_5(),yamld.getS2CCLK_6(),yamld.getS2CCLK_7(),yamld.getS2CCLK_8(),yamld.getfpga1_bitfile(),yamld.getfpga2_bitfile(),fpga1outpath,fpga2outpath)
-#     # while(program_retry<4):
-#     #     return_code=auto.loopfunction(program_retry)
-#     #     if return_code==0:
-#     #         break
-#     #     else:
-#     #         autofunc_offpow.offpower(powermoduleip,fpga1outpath,fpga2outpath)
-#     #         program_retry+=1
+# #     # program_retry=1
+# #     # auto=automationmain(modulename,hostname,powermoduleip,listvar,fpga1outpath,fpga2outpath)
+# #     # #auto=automationmain(powermoduleip,hostname,yamld.getpower_1_8V(),yamld.getpower_3_3V(),yamld.getpower_5_0V(),yamld.getS2CCLK_1(),yamld.getS2CCLK_2(),yamld.getS2CCLK_3(),yamld.getS2CCLK_4(),yamld.getS2CCLK_5(),yamld.getS2CCLK_6(),yamld.getS2CCLK_7(),yamld.getS2CCLK_8(),yamld.getfpga1_bitfile(),yamld.getfpga2_bitfile(),fpga1outpath,fpga2outpath)
+# #     # while(program_retry<4):
+# #     #     return_code=auto.loopfunction(program_retry)
+# #     #     if return_code==0:
+# #     #         break
+# #     #     else:
+# #     #         autofunc_offpow.offpower(powermoduleip,fpga1outpath,fpga2outpath)
+# #     #         program_retry+=1
