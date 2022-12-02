8e97ef79ff46714c7888033191cb2e35ae418513
Yong Han <yonghanlee@starfive.com>
2022-12-02 22:44:46 +0800

    ok

--- maintest.py
+++ maintest.py
@@ -13,7 +13,6 @@ if __name__ == '__main__':
     hostname=yamld.gethostname()
     powermoduleip=yamld.getpowermodule_ip()
     listvar=[yamld.getpower_1_8V(),yamld.getpower_3_3V(),yamld.getpower_5_0V(),yamld.getS2CCLK_1(),yamld.getS2CCLK_2(),yamld.getS2CCLK_3(),yamld.getS2CCLK_4(),yamld.getS2CCLK_5(),yamld.getS2CCLK_6(),yamld.getS2CCLK_7(),yamld.getS2CCLK_8(),yamld.getfpga1_bitfile(),yamld.getfpga2_bitfile()]
-    print(powermoduleip)
     mk=mkfolfildir(yamld.getfpga1_bitfile(),yamld.getfpga2_bitfile())
     fpga1outpath,fpga2outpath=mk.mainff()
     
